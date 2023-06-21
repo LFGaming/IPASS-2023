cf074f2052418ac0ab2ef030f90c6fa5e8485d45
LFGaming (Luke) <github@luke.irtl.nl>
2023-06-21 16:46:22 +0200

    Update BMP280.cpp
    now using thechwlib::i2c_read_transaction
    and hwlib::i2c_write_transaction

--- src/BMP280.cpp
+++ src/BMP280.cpp
@@ -37,10 +37,17 @@ void BMP280::read_calibration_data() {
 }
 
 float BMP280::readTemperature() {
-    BMP280::write_start();
-    bus.write(BMP280_ADDRESS).write(BMP280_REG_TEMP_MSB);
-    uint8_t msb = bus.read(BMP280_ADDRESS).read_byte();
-    uint8_t lsb = bus.read(BMP280_ADDRESS).read_byte();
+    uint8_t msb, lsb;
+
+    // Create I2C write transaction
+    hwlib::i2c_write_transaction writetrans = bus.write(BMP280_ADDRESS);
+    writetrans.write(BMP280_REG_TEMP_MSB);
+
+    // Create I2C read transaction
+    hwlib::i2c_read_transaction readtrans = bus.read(BMP280_ADDRESS);
+    msb = readtrans.read_byte();
+    lsb = readtrans.read_byte();
+
     BMP280::write_nack();
     BMP280::write_stop();
 
@@ -51,46 +58,48 @@ float BMP280::readTemperature() {
         ((adcValue >> 4) - (static_cast<int32_t>(calibrationData.dig_T1)))) >> 12) *
         (static_cast<int32_t>(calibrationData.dig_T3))) >> 14;
     int32_t t_fine = var1 + var2;
-    float T = (t_fine * 5 + 126) >> 8;
-    return T / 100.0f;
-    // calibrationData.t_fine = var1 + var2; // Assign t_fine to the calibrationData member
+    float T = (t_fine * 5 + 126) / 256.0f;
+    return T;
 }
 
 float BMP280::readPressure() {
-    int64_t var1, var2, p;
+    uint8_t msb, lsb, xlsb;
 
     // Must be done first to get the t_fine variable set up
     int32_t t_fine = readTemperature();
 
-    bus.write(BMP280_ADDRESS).write(BMP280_REGISTER_PRESSUREDATA); // Register address for pressure data
-    uint8_t msb = bus.read(BMP280_ADDRESS).read_byte();
-    uint8_t lsb = bus.read(BMP280_ADDRESS).read_byte();
-    uint8_t xlsb = bus.read(BMP280_ADDRESS).read_byte();
+    // Create I2C write transaction
+    hwlib::i2c_write_transaction writetrans = bus.write(BMP280_ADDRESS);
+    writetrans.write(BMP280_REGISTER_PRESSUREDATA);
+
+    // Create I2C read transaction
+    hwlib::i2c_read_transaction readtrans = bus.read(BMP280_ADDRESS);
+    msb = readtrans.read_byte();
+    lsb = readtrans.read_byte();
+    xlsb = readtrans.read_byte();
 
     int32_t adc_P = (static_cast<int32_t>(msb) << 16) | (static_cast<int32_t>(lsb) << 8) | (static_cast<int32_t>(xlsb) >> 4);
+    adc_P >>= 4;
 
-//   int32_t adc_P = read24(BMP280_REGISTER_PRESSUREDATA);
-  adc_P >>= 4;
-
-  var1 = ((int64_t)t_fine) - 128000;
-  var2 = var1 * var1 * (int64_t)calibrationData.dig_P6;
-  var2 = var2 + ((var1 * (int64_t)calibrationData.dig_P5) << 17);
-  var2 = var2 + (((int64_t)calibrationData.dig_P4) << 35);
-  var1 = ((var1 * var1 * (int64_t)calibrationData.dig_P3) >> 8) +
+    int64_t var1, var2, p;
+    var1 = ((int64_t)t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)calibrationData.dig_P6;
+    var2 = var2 + ((var1 * (int64_t)calibrationData.dig_P5) << 17);
+    var2 = var2 + (((int64_t)calibrationData.dig_P4) << 35);
+    var1 = ((var1 * var1 * (int64_t)calibrationData.dig_P3) >> 8) +
          ((var1 * (int64_t)calibrationData.dig_P2) << 12);
-  var1 =
-      (((((int64_t)1) << 47) + var1)) * ((int64_t)calibrationData.dig_P1) >> 33;
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)calibrationData.dig_P1) >> 33;
 
-  if (var1 == 0) {
-    return 0; // avoid exception caused by division by zero
-  }
-  p = 1048576 - adc_P;
-  p = (((p << 31) - var2) * 3125) / var1;
-  var1 = (((int64_t)calibrationData.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
-  var2 = (((int64_t)calibrationData.dig_P8) * p) >> 19;
+    if (var1 == 0) {
+        return 0; // avoid exception caused by division by zero
+    }
+    p = 1048576 - adc_P;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)calibrationData.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)calibrationData.dig_P8) * p) >> 19;
 
-  p = ((p + var1 + var2) >> 8) + (((int64_t)calibrationData.dig_P7) << 4);
-  return (float)p / 256;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)calibrationData.dig_P7) << 4);
+    return static_cast<float>(p) / 256.0f;
 }
 
 
